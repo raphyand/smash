12b247443c47e40c5b359eaf1569d97467289adb
Raphael S. Andaya <raphyand@gmail.com>
2021-06-22 21:46:12 -0700

    Have a functional, working run.  Byte limit is 51, with 52 causing a segmentation fault

--- part-1/run_cmd.cc
+++ part-1/run_cmd.cc
@@ -11,9 +11,15 @@ using namespace std;
 /// \param s The string to repeat, must be 1 byte or larger.
 /// \returns the string \p s repeated \p n_repeat times
 string BigString(int n_repeat, const string& s) {
-  return string("foo");
+  string x = s;
+  for (int i = 0; i < n_repeat; i++){
+    x += s; 
+  }
+  return x;
+  // return string("foo");
 }
 
+// ./run_cmd ./overflow whatever_string_to_use
 /// Entry point to the test harness which repeated executes the
 /// program provided as an argument with arguments that increase in
 /// length. Reports when the target program exits with an abnormal
@@ -29,15 +35,20 @@ int main(int argc, char const* argv[]) {
   string base_str = string(argv[2]);
   int rv = 0;
   int n_repeat = 0;
-  string repeated = BigString(n_repeat, base_str);
-  // Concatenate the command and the repeated string as a
-  // single string. Redirect all output to /dev/null to run
-  // the program silently. We are only interested in when
-  // the target program fails.
-  string cmd = base_cmd + " " + repeated + " > /dev/null";
-  // cout << "Running the command: " << cmd << "\n";
-  cout << "Trying " << repeated.size() << " bytes. ";
-  rv = system(cmd.c_str());
-  cout << "Exited with status " << rv << "\n";
+  do{
+    string repeated = BigString(n_repeat, base_str);
+    n_repeat++;
+    // Concatenate the command and the repeated string as a
+    // single string. Redirect all output to /dev/null to run
+    // the program silently. We are only interested in when
+    // the target program fails.
+    // Run it repeatedly until it exit does return 0
+    string cmd = base_cmd + " " + repeated + " > /dev/null";
+    cout << "Running the command: " << cmd << "\n";
+    cout << "Trying " << repeated.size() << " bytes. ";
+    cout << endl << repeated;
+    rv = system(cmd.c_str());
+    cout << "Exited with status " << rv << "\n";
+  }while(rv == 0);
   return 0;
 }
